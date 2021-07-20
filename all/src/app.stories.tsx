96133be91603344e7832f2e11db5ec8f440d52f5
turner-kl <angra5296@gmail.com>
2021-07-20 11:15:51 +0900

    done

---
+++ all/src/app.stories.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { App } from "./app";
+
+export default {
+  title: "App",
+  component: App,
+};
+
+export const Component = () => <App />;
+
+Component.story = {
+  name: "Components",
+};
